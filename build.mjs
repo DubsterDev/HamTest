d294e0962972fd2434c4aa5e467f2247749413b5
DubsterDev <317384959+DubsterDev@users.noreply.github.com>
2026-08-21 16:52:09 -0500

    Make the header a template

--- build.mjs
+++ build.mjs
@@ -4,12 +4,15 @@ import { readFile, writeFile, rm, mkdir, cp } from "node:fs/promises";
 await rm("build/", { recursive: true, force: true });
 await mkdir("build/");
 
+// Read header
+const header = await readFile("app/header.html", {encoding: "utf-8"});
+
 // Copy assets
 await cp("app/assets", "build/assets", { recursive: true });
 
 // Copy 404, index
-await cp("app/index.html", "build/index.html");
-await cp("app/404.html", "build/404.html");
+await copyFile("app/index.html", "build/index.html");
+await copyFile("app/404.html", "build/404.html");
 
 const classNames = ["Technician", "General", "Extra"];
 
@@ -22,13 +25,37 @@ const practiceTemplate = await readFile("app/practice.html", {encoding: "utf-8"}
 const studyTemplate = await readFile("app/study.html", {encoding: "utf-8"});
 classNames.forEach(async className => {
     const lowerClassName = className.toLowerCase();
-    
-    const practiceTemplateFilled = practiceTemplate
-        .replaceAll("{{upper_class}}", className)
-        .replaceAll("{{lower_class}}", lowerClassName);
-    const studyTemplateFilled = studyTemplate
-        .replaceAll("{{upper_class}}", className)
-        .replaceAll("{{lower_class}}", lowerClassName);
-    await writeFile(`build/practice/${lowerClassName}.html`, practiceTemplateFilled);
-    await writeFile(`build/study/${lowerClassName}.html`, studyTemplateFilled);
+    const replacements = {
+        "upper_class": className,
+        "lower_class": lowerClassName
+    };
+
+    await copyFile("app/practice.html", `build/practice/${lowerClassName}.html`, replacements);
+    await copyFile("app/study.html", `build/study/${lowerClassName}.html`, replacements);
 })
+
+async function copyFile(src, dest, customReplace={}) {
+    const template = await readFile(src, {encoding: "utf-8"});
+    const splitAtMetadata = template.split(":endmetadata-->");
+
+    const metadata = JSON.parse(splitAtMetadata[0].replace("<!--metadata:", "").trim());
+
+    let extraCss = "";
+    if ("css" in metadata) {
+        metadata["css"].forEach(stylesheet => {
+            extraCss += `<link rel="stylesheet" href="/assets/styles/${stylesheet}.css">\n`
+        })
+    }
+    const newHeader = header
+        .replaceAll("{{title}}", metadata["title"])
+        .replaceAll("{{extra_css}}", extraCss)
+
+    
+    let processedTemplate = splitAtMetadata[1].replaceAll("{{header}}", newHeader);
+
+    Object.keys(customReplace).forEach(key => {
+        processedTemplate = processedTemplate.replaceAll(`{{${key}}}`, customReplace[key]);
+    })
+
+    await writeFile(dest, processedTemplate);
+}
